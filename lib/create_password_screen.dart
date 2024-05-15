9968af33104193d5b06f8b3e6d066d2edad54f0d
awanish321 <awanish237@gmail.com>
2024-05-15 17:50:04 +0530

    Initial commit

---
+++ lib/create_password_screen.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:iconsax/iconsax.dart';
+
+class CreatePasswordScreen extends StatefulWidget {
+  const CreatePasswordScreen({super.key});
+
+  @override
+  State<CreatePasswordScreen> createState() => _CreatePasswordScreenState();
+}
+
+class _CreatePasswordScreenState extends State<CreatePasswordScreen> {
+  final username = TextEditingController();
+  final email = TextEditingController();
+  GlobalKey<FormState> studentFormKey = GlobalKey<FormState>();
+  final password = TextEditingController();
+  final confirmPassword = TextEditingController();
+  bool _obscureText = true;
+  bool _obscureText1 = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Back Button
+                InkWell(
+                  borderRadius: BorderRadius.circular(100),
+                  onTap: () {},
+                  child: Container(
+                      height: 45,
+                      width: 45,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey.withOpacity(0.2)),
+                      child: const Center(
+                          child: Icon(
+                            Icons.arrow_back_ios_sharp,
+                            size: 25,
+                            color: Colors.blue,
+                          ))),
+                ),
+
+                // Logo Icon
+                Center(
+                    child: SvgPicture.asset(
+                      "assets/logoipsum-297.svg",
+                      height: 30,
+                      width: 30,
+                    )),
+
+                // Image
+                Center(
+                    child: Image.asset(
+                      "assets/clip-reading-of-books-1.png",
+                      height: 200,
+                    )),
+
+                // Text
+                const Text(
+                  "Create a New Password",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  "Enter a new password to change your forgotten password",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                ),
+
+                const SizedBox(height: 16),
+                // TextFormFields
+                Form(
+                  key: studentFormKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: password,
+                        obscureText: _obscureText,
+                        decoration: InputDecoration(
+                          labelText: 'Create Password',
+                          hintText: 'Enter your password',
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(color: Colors.cyanAccent),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: Colors.grey.withOpacity(0.5)),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          prefixIcon: Icon(Icons.lock_outline_rounded),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              color: Colors.black,
+                              _obscureText ? Iconsax.eye_slash : Iconsax.eye,
+                            ),
+
+                            onPressed: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                          ),
+
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: confirmPassword,
+                        obscureText: _obscureText1,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                          hintText: 'Re-enter your password',
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(color: Colors.cyanAccent),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: Colors.grey.withOpacity(0.5)),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          prefixIcon: const Icon(Icons.lock_outline_rounded),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              color: Colors.black,
+                              _obscureText1 ? Iconsax.eye_slash : Iconsax.eye,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _obscureText1 = !_obscureText1;
+                              });
+                            },
+                          ),
+
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please confirm your password';
+                          }
+                          if (value != password.text) {
+                            return 'Passwords do not match';
+                          }
+                          return null;
+                        },
+                      ),
+
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 16,),
+
+                // Login Button
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
+                      ),
+                      onPressed: (){}, child: const Text("Change Password", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),)),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login_with_username_or_mobile.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginWithOTPScreen extends StatefulWidget {
+  const LoginWithOTPScreen({super.key});
+
+  @override
+  State<LoginWithOTPScreen> createState() => _LoginWithOTPScreenState();
+}
+
+class _LoginWithOTPScreenState extends State<LoginWithOTPScreen> {
+  final username = TextEditingController();
+  final email = TextEditingController();
+  GlobalKey<FormState> studentFormKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Back Button
+                InkWell(
+                  borderRadius: BorderRadius.circular(100),
+                  onTap: () {},
+                  child: Container(
+                      height: 45,
+                      width: 45,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.grey.withOpacity(0.2)),
+                      child: const Center(
+                          child: Icon(
+                            Icons.arrow_back_ios_sharp,
+                            size: 25,
+                            color: Colors.blue,
+                          ))),
+                ),
+
+                // Logo Icon
+                Center(
+                    child: SvgPicture.asset(
+                      "assets/logoipsum-297.svg",
+                      height: 30,
+                      width: 30,
+                    )),
+
+                // Image
+                Center(
+                    child: Image.asset(
+                      "assets/clip-reading-of-books-1.png",
+                      height: 200,
+                    )),
+
+                // Text
+                const Text(
+                  "Login as Student",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+
+                const SizedBox(height: 16),
+                // TextFormFields
+                Form(
+                  key: studentFormKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: username,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.mail_outline_rounded,
+                            color: Colors.grey,
+                          ),
+                          labelText: 'username/mobile',
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(color: Colors.cyanAccent),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: Colors.grey.withOpacity(0.5)),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Enter your name.';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 16,),
+
+                // Login Button
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
+                      ),
+                      onPressed: (){}, child: const Text("Send OTP", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),)),
+                ),
+                Center(child: TextButton(onPressed: (){}, child: const Text("Login with Password", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),))),
+                const SizedBox(height: 16,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 1.5,
+                      width: 100,
+                      color: Colors.grey.withOpacity(0.3),
+                    ),
+                    const SizedBox(width: 7,),
+                    const Text("or", style: TextStyle(color: Colors.grey, fontSize: 14),),
+                    const SizedBox(width: 7,),
+                    Container(
+                      height: 1.5,
+                      width: 100,
+                      color: Colors.grey.withOpacity(0.3),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16,),
+                const Center(child: Text("sign in with", style: TextStyle(fontWeight: FontWeight.w500),)),
+                const SizedBox(height: 16,),
+
+                // SIGN IN WITH GOOGLE AND FACEBOOK
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      // borderRadius: ,
+                        child: Image.asset("assets/search.png", height: 40,)),
+                    const SizedBox(width: 40,),
+                    InkWell(child: Image.asset("assets/facebook.png", height: 40,))
+                  ],
+                ),
+                const SizedBox(height: 32,),
+                const Center(child: Text("Don't have an account?")),
+                const SizedBox(height: 10,),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: TextButton(
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            side: const BorderSide(color: Colors.blue, width: 1.5),
+                            borderRadius: BorderRadius.circular(10)),
+
+                      ),
+                      onPressed: (){}, child: const Text("Create an account", style: TextStyle(fontSize: 15, color: Colors.blue, fontWeight: FontWeight.bold),)),
+                ),
+
+                const SizedBox(height: 20,)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
